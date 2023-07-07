68607489fe7f323805a0b3a99a233ed855325e02
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-07-07 09:11:43 -0500

    complete python loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
-
-def square_integers(int_list):
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+def reverse_string(str):
+    reversed_str = ""
+    for i in range(len(str) - 1, -1, -1):
+        reversed_str += str[i]
+    return reversed_str
+
+def square_integers(int_list):
+    return [num ** 2 for num in int_list]
